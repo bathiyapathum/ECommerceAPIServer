11e0e6600af91a44b1bb17d0e9f05a63c0d99f97
naveenm99 <m.naveen@gtngroup.com>
2024-09-29 23:53:28 +0530

    Created order managing flow

---
+++ ECommerceAPI.Application/Features/OrderService.cs
@@ -0,0 +1,75 @@
+﻿using ECommerceAPI.Application.DTOs;
+using ECommerceAPI.Application.Interfaces;
+using ECommerceAPI.Core.Entities;
+using ECommerceAPI.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features
+{
+    internal class OrderService : IOrderService
+    {
+        private readonly  OrderRepository _orderRepository;
+
+        public OrderService(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;        
+        }
+
+        public Task CancelOrderAsync(string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task CreateOrderAsync(OrderDTO orderDTO)
+        {
+            try
+            {
+                var order = new Order
+                {
+                    OrderId = Guid.NewGuid().ToString(),
+                    CustomerId = orderDTO.CustomerId,
+                    Items = orderDTO.Items,
+                    CreatedAt = DateTime.UtcNow,
+                    DeliveredAt = orderDTO.DeliveredAt,
+                    DispatchedAt = orderDTO.DispatchedAt,
+                    Status = orderDTO.Status,
+                };
+                await _orderRepository.CreateAsync(order);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Task DeleteOrderAsync(string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Order> GetOrderAsync(string orderId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Order>> GetOrdersByCustomerAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderDetailsAsync(OrderDTO orderDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOrderStatusAsync(string orderId, string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ECommerceAPI.Core/Entities/Order.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+namespace ECommerceAPI.Core.Entities
+{
+    [FirestoreData]
+    public class Order
+    {
+        [FirestoreProperty("orderId")]
+        public string OrderId { get; set; }
+
+        [FirestoreProperty("customerId")]
+        public string CustomerId { get; set; }
+
+        [FirestoreProperty("items")]
+        public List<OrderItem> Items { get; set; }
+
+        [FirestoreProperty("status")]
+        public string Status { get; set; } // Processing, Delivered, Canceled, etc.
+
+        [FirestoreProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [FirestoreProperty("dispatchedAt")]
+        public DateTime? DispatchedAt { get; set; }
+
+        [FirestoreProperty("DeliceredAt")]
+        public DateTime? DeliveredAt { get; set; }
+    }
+
+    [FirestoreData]
+    public class OrderItem 
+    {
+        [FirestoreProperty]
+        public string ProductId { get; set; }
+
+        [FirestoreProperty]
+        public string ProductName { get; set; }
+
+        [FirestoreProperty]
+        public string VendorId { get; set; }
+
+        [FirestoreProperty]
+        public int Quantity { get; set; }
+
+        [FirestoreProperty]
+        public string Price { get; set; }
+    }
+}

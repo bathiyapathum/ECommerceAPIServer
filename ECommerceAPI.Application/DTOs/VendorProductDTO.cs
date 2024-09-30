1c687ad137900027fe9a21176d3b5643aec2778c
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-09-30 19:11:13 +0530

    Stock Status

--- ECommerceAPI.Application/DTOs/VendorProductDTO.cs
+++ ECommerceAPI.Application/DTOs/VendorProductDTO.cs
@@ -13,5 +13,6 @@ namespace ECommerceAPI.Application.DTOs
         public string VendorId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public string StockStatus { get; set; }  // New field for StockStatus
     }
 }
